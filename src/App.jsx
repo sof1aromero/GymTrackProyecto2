f3bcfebd8afbcbe490324da31c64ce0db5a2307a
Julianaa <juliana.angarita.ch.2203@gmail.com>
2025-07-01 17:01:21 -0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { Routes, Route, Router } from "react-router-dom";
-
-import './App.css'
-import NavBar from '../src/components/ServiciosNotificacionesMas/NavBar'
-import Footer from '../src/components/ServiciosNotificacionesMas/Footer'
-import  "./styles/estilos/LAYOUTT.css"
+import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import NavBar from "./components/InicioSesionFN/NavBar";
 import Registro from "./components/InicioSesion/Registro";
 import RecuperarContrasena from "./components/InicioSesion/RecuperarContrasena";
 import IniciarSesion from "./components/InicioSesion/IniciarSesion";
@@ -12,14 +9,15 @@ import Footer from "./components/InicioSesionFN/Footer";
 import CalendarioCliente from "./components/ClienteEntrenador/CalendarioCliente";
 import HistorialClases from "./components/ClienteEntrenador/HistorialClases";
 import ConfirmacionReserva from "./components/ClienteEntrenador/ConfirmacionReserva";
-import NavBarPriv from "./components/InicioSesionFN/NavBarPriv";
+import MisServicios from "./components/ServiciosNotificaciones/MisServicios";
+import NavBarPriv from "./components/ServiciosNotificaciones/NavBarPriv";
 import DashboardAdmin from './components/Administrador/DashboardAdmin';
-
 
 function App() {
   return (
     <>
-      <NavBar />
+<NavBar />
+
       <Routes>
         <Route path="/" element={<IniciarSesion />} />
         <Route path="/registro" element={<Registro />} />
@@ -29,11 +27,10 @@ function App() {
         <Route path="/calendario" element={<CalendarioCliente />} />
         <Route path="/historial-clases" element={<HistorialClases />} />
         <Route path="/confirmacion-reserva" element={<ConfirmacionReserva/>} />
+        <Route path="/mis-servicios" element={<MisServicios />} />
         <Route path="/admin" element={<DashboardAdmin />} />
-
-        <Route element={<NavBarPriv />}/>
-      </Routes>
-
+        </Routes>
+        /*hola/*
       <Footer />
     </>
   );
